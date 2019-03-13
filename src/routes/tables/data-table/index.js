e58951e11f2602d96ece0a5b7aa5c338cebb797f
Ata_R <ata.razavi892gmail.com>
2019-03-14 01:01:58 +0330

    data-table all except group option

--- src/routes/tables/data-table/index.js
+++ src/routes/tables/data-table/index.js
@@ -27,176 +27,62 @@ class DataTable extends React.Component {
 	}
 
 	componentDidMount = () => {		
-		// (async () => {
-		// const rawResponse = await fetch(AppConfig.baseURL + '/permission/user/filter', {
-		// 	method: 'POST',
-		// 	headers: {
-		// 		'Content-Type': 'application/json',
-		// 		'Authorization': 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiIsImlzcyI6Imh0dHA6Ly9wYXMua2lzaGlzYy5jby5pciIsImlhdCI6MTU1MTk2ODU3MSwiZXhwIjoxNTUxOTg2NTcxfQ.CBgJ-W0AMINGHxhQUcBxwzsOAqpTl5BS3LB0t0L5IRg'
-		// 	},
-		// 	body: JSON.stringify({
-		// 		"fromDate": "",
-		// 		"groupId": 0,
-		// 		"needPaginate": true,
-		// 		"pageNumber": 0,
-		// 		"pageSize": 10,
-		// 		"productProviderId": 76,
-		// 		"resultSize": 0,
-		// 		"roleId": 0,
-		// 		"toDate": "",
-		// 		"termToFind": ""
-		// 	  })
-		// });
-		// const content = await rawResponse.json();
-		// console.log(content);
-		// if (content.status == 200 ){
-		// 	console.log('success');
-		// }
-		// })();
-		const the_data = {
-			"status": 200,
-			"message": "success",
-			"token": "",
-			"result": {
-				"messageModel": {
-					"type": "success",
-					"title": null,
-					"text": "درخواست با موفقیت آمیز انجام گرفت",
-					"messagesKey": "requestSuccess",
-					"violations": null
-				},
-				"paginateModel": {
-					"pages": [],
-					"currentPage": 0,
-					"pageSize": 10
-				},
-				"dtos": [
-					{
-						"id": 185,
-						"username": "test-user2",
-						"password": "$2a$10$kRZE52.oAzZ55MgTA8nK2eNsjw9BuH8FRYhlMVr99xcABLys8XkRC",
-						"password2": null,
-						"description": "test-user2",
-						"fullName": "test user 2",
-						"email": "test-user2@gmail.com",
-						"mobile": null,
-						"creationDate": "1397/11/14 13:3",
-						"editionDate": null,
-						"productproviderDTO": {
-							"id": 76,
-							"productproviderLangDTOS": [
-								{
-									"productProviderId": 76,
-									"name": "چونک",
-									"companyName": "چونک ستر ایران",
-									"mainOfficeAddress": "آدرس چونک",
-									"mainOfficeTel": "454654",
-									"mainOfficeFax": "646546",
-									"managerFullname": null,
-									"describtion": "توضیحات چونک",
-									"languageDTO": {
-										"id": 1,
-										"code": "fa",
-										"title": "فارسی"
-									}
-								},
-								{
-									"productProviderId": 76,
-									"name": "choonak",
-									"companyName": "Choonak goster",
-									"mainOfficeAddress": "Address choonak",
-									"mainOfficeTel": "454654",
-									"mainOfficeFax": "646546",
-									"managerFullname": null,
-									"describtion": "choonak desc",
-									"languageDTO": {
-										"id": 2,
-										"code": "en",
-										"title": "English"
-									}
-								}
-							]
+		(async () => {
+		const rawResponse = await fetch(AppConfig.baseURL + '/permission/user/filter', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'Authorization': giventoken
+			},
+			body: JSON.stringify({
+				"fromDate": "",
+				"groupId": 0,
+				"needPaginate": true,
+				"pageNumber": 0,
+				"pageSize": 10,
+				"productProviderId": 76,
+				"resultSize": 0,
+				"roleId": 0,
+				"toDate": "",
+				"termToFind": ""
+			  })
+			});
+			const response = await rawResponse.json();
+			console.log(response);
+			if (response.status == 200 ){
+				console.log('success');
+				
+				// it should be gotten from sth like localstorage
+				const currentlanguage = 'fa'
+				// it should be gotten from sth like localstorage
+				let nameinCurrentLang = {}
+				const theList = response.result.dtos.map(each => {			
+					each.productproviderDTO.productproviderLangDTOS.map(eachlang => {
+						if (currentlanguage == eachlang.languageDTO.code){
+							nameinCurrentLang = {
+								lang_code: eachlang.languageDTO.code,
+								nameinthisLang: eachlang.name,
+								companynameinthislang: eachlang.companyName,
+								descriptioninthislang: eachlang.describtion
+							}
 						}
-					},
-					{
-						"id": 165,
-						"username": "user_test",
-						"password": "$2a$10$rxHoy6LXZwW0x7Kr3wD1n.wBAkPbfM4ka2Ks19EIDsr/t2fGTPhMq",
-						"password2": null,
-						"description": null,
-						"fullName": "User Test",
-						"email": "user_test@kisc.co.ir",
-						"mobile": null,
-						"creationDate": "1397/11/10 12:46",
-						"editionDate": null,
-						"productproviderDTO": {
-							"id": 76,
-							"productproviderLangDTOS": [
-								{
-									"productProviderId": 76,
-									"name": "چونک",
-									"companyName": "چونک ستر ایران",
-									"mainOfficeAddress": "آدرس چونک",
-									"mainOfficeTel": "454654",
-									"mainOfficeFax": "646546",
-									"managerFullname": null,
-									"describtion": "توضیحات چونک",
-									"languageDTO": {
-										"id": 1,
-										"code": "fa",
-										"title": "فارسی"
-									}
-								},
-								{
-									"productProviderId": 76,
-									"name": "choonak",
-									"companyName": "Choonak goster",
-									"mainOfficeAddress": "Address choonak",
-									"mainOfficeTel": "454654",
-									"mainOfficeFax": "646546",
-									"managerFullname": null,
-									"describtion": "choonak desc",
-									"languageDTO": {
-										"id": 2,
-										"code": "en",
-										"title": "English"
-									}
-								}
-							]
-						}
-					}
-				]
+					})
+					return({
+						id: each.id,
+						username: each.username,
+						name: each.fullName,
+						productOwnerID: each.productproviderDTO.id,
+						productOwnerName: nameinCurrentLang.nameinthisLang,
+						userState: each.editionDate, //shouldbe sth like each.UserState
+						email: each.email,
+					})
+				})
+				
+				this.setState(
+					{theuserslist: theList}
+				)
 			}
-		}
-		// it should be gotten from sth like localstorage
-		const currentlanguage = 'fa'
-		// it should be gotten from sth like localstorage
-		let nameinCurrentLang = {}
-		const theList = the_data.result.dtos.map(each => {			
-			each.productproviderDTO.productproviderLangDTOS.map(eachlang => {
-				if (currentlanguage == eachlang.languageDTO.code){
-					nameinCurrentLang = {
-						lang_code: eachlang.languageDTO.code,
-						nameinthisLang: eachlang.name,
-						companynameinthislang: eachlang.companyName,
-						descriptioninthislang: eachlang.describtion
-					}
-				}
-			})
-			
-			return({
-				id: each.id,
-				username: each.username,
-				name: each.fullName,
-				productOwnerID: each.productproviderDTO.id,
-				productOwnerName: nameinCurrentLang.nameinthisLang,
-				userState: each.editionDate, //shouldbe sth like each.UserState
-				email: each.email,
-			})
-		})
-		
-		this.setState(
-			{theuserslist: theList})
+		})();
 		}
 
 	actionClickhandler = (id, uname, action) => {
