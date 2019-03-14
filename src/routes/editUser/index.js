473f26fac4e7988f598efc9192fc805036dc1965
Ata_R <ata.razavi892gmail.com>
2019-03-14 04:39:35 +0330

    edit user page

--- src/routes/editUser/index.js
+++ src/routes/editUser/index.js
@@ -23,6 +23,9 @@ import RctCollapsibleCard from 'Components/RctCollapsibleCard/RctCollapsibleCard
 // intl messages
 import IntlMessages from 'Util/IntlMessages';
 
+// app config
+import AppConfig from '../../constants/AppConfig';
+
 const giventoken = localStorage.getItem('given_token')
 
 export default class AutoComplete extends Component {
@@ -35,210 +38,82 @@ export default class AutoComplete extends Component {
 		mobileNumber: '',
 		description: '',
         productownerid: 0,
-        productownerName: '',
+		productownerName: '',
+		suggestions: [],
 		userState : 'active'
 	}
 
     componentDidMount = () => {      
-        // (async () => {
-        //     const rawResponse = await fetch(AppConfig.baseURL + '/permission/user/findbyid', {
-        //         method: 'POST',
-        //         headers: {
-        //             'Content-Type': 'application/json',
-        //             'Authorization': givenToken
-        //         },
-        //         body: JSON.stringify({
-        //             "id": this.props.location.state.user_id
-        //         })
-        //     });
-        //     const response = await rawResponse.json();
-        //     console.log(response);
-        //     if (response.status == 200 ){
-        //         console.log('success');
-        //         this.setState({
-        //             username: response.result.dto.username,
-        //             familyanme: response.result.dto.fullName,
-        //             email: response.result.dto.email,
-        //             mobileNumber: response.result.dto.mobile,
-        //             description: response.result.dto.description,
-        //             productownerid: response.result.dto.productproviderDTO.id,
-        //         })
-        //     }
-        // })();
-
-        let response = {
-            "status": 200,
-            "message": "success",
-            "token": "",
-            "result": {
-                "messageModel": {
-                    "type": "success",
-                    "title": null,
-                    "text": "درخواست با موفقیت آمیز انجام گرفت",
-                    "messagesKey": "requestSuccess",
-                    "violations": null
+        (async () => {
+            const rawResponse = await fetch(AppConfig.baseURL + '/permission/user/findbyid', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': giventoken
                 },
-                "dto": {
-                    "id": 145,
-                    "username": "admin",
-                    "password": "$2a$10$ebknTc82ylO8KtYloFx8Te3mB.EcgXe4N4ex0o9OgAy0KDKphJS56",
-                    "password2": null,
-                    "description": "توضیحات",
-                    "fullName": "مدیر سیستم",
-                    "email": "pas@kisc.co.ir",
-                    "mobile": "09388926636",
-                    "creationDate": "1396/06/19 10:53",
-                    "editionDate": "1397/04/14 8:26",
-                    "productproviderDTO": {
-                        "id": null,
-                        "productproviderLangDTOS": [
-                            {
-                                "productProviderId": 76,
-                                "name": "چونک",
-                                "companyName": "چونک ستر ایران",
-                                "mainOfficeAddress": "آدرس چونک",
-                                "mainOfficeTel": "454654",
-                                "mainOfficeFax": "646546",
-                                "managerFullname": null,
-                                "describtion": "توضیحات چونک",
-                                "languageDTO": {
-                                    "id": 1,
-                                    "code": "fa",
-                                    "title": "فارسی"
-                                }
-                            },
-                            {
-                                "productProviderId": 76,
-                                "name": "choonak",
-                                "companyName": "Choonak goster",
-                                "mainOfficeAddress": "Address choonak",
-                                "mainOfficeTel": "454654",
-                                "mainOfficeFax": "646546",
-                                "managerFullname": null,
-                                "describtion": "choonak desc",
-                                "languageDTO": {
-                                    "id": 2,
-                                    "code": "en",
-                                    "title": "English"
-                                }
-                            }
-                        ]
-                    }
-                }
-            }
-        }
-        
-
-		// it should be gotten from sth like localstorage
-		const currentlanguage = 'fa'
-		// it should be gotten from sth like localstorage
-		let nameinCurrentLang = {}
-		response.result.dto.productproviderDTO.productproviderLangDTOS.map(each => {	
-            if (currentlanguage == each.languageDTO.code){
-                nameinCurrentLang = {
-                    lang_code: each.languageDTO.code,
-                    nameinthisLang: each.name,
-                    companynameinthislang: each.companyName,
-                    descriptioninthislang: each.describtion
-                }
-            }
-        })
-
-        this.setState({
-            username: response.result.dto.username,
-            familyanme: response.result.dto.fullName,
-            email: response.result.dto.email,
-            mobileNumber: response.result.dto.mobile,
-            description: response.result.dto.description,
-            productownerid: response.result.dto.productproviderDTO.id,
-            productownerName: nameinCurrentLang.nameinthisLang
-        })
-    } 
-
-	beforeMount = () => {
-		
-    // (async () => {
-    //   const rawResponse = await fetch(AppConfig.baseURL + '/product/provider/filter', {
-    //       method: 'POST',
-    //       headers: {
-    //         'Content-Type': 'application/json',
-    //         'Authorization': givenToken
-    //       },
-    //       body: JSON.stringify({
-    //         "fromDate": "",
-    //         "needPaginate": true,
-    //         "pageNumber": 0,
-    //         "pageSize": 10,
-    //         "resultSize": 0,
-    //         "termToFind": "",
-    //         "toDate": ""
-    //       })
-    //   });
-    //   const content = await rawResponse.json();
-    //   console.log(content);
-    //   if (content.status == 200 ){
-    //       console.log('success');
-    //       let dtos_ = content.result.dtos
-    //       suggestions = dtos_.map(eachOwner => {
-    //         return({label: eachOwner.name, value: eachOwner.name, id: eachOwner.productProviderId})
-    //       })
-    //   }
-    // })();
-		let the_data = {
-			"status": 200,
-			"message": "success",
-			"token": "",
-			"result": {
-				"messageModel": {
-					"type": "success",
-					"title": null,
-					"text": "درخواست با موفقیت آمیز انجام گرفت",
-					"messagesKey": "requestSuccess",
-					"violations": null
-				},
-				"paginateModel": {
-					"pages": [],
-					"currentPage": 0,
-					"pageSize": 10
-				},
-				"dtos": [
-					{
-						"productProviderId": 76,
-						"name": "چونک",
-						"companyName": "چونک ستر ایران",
-						"mainOfficeAddress": "آدرس چونک",
-						"mainOfficeTel": "454654",
-						"mainOfficeFax": "646546",
-						"managerFullname": null,
-						"describtion": "توضیحات چونک",
-						"languageDTO": {
-							"id": 1,
-							"code": "fa",
-							"title": "فارسی"
-						}
-					},
-					{
-						"productProviderId": 77,
-						"name": "choonak",
-						"companyName": "Choonak goster",
-						"mainOfficeAddress": "Address choonak",
-						"mainOfficeTel": "454654",
-						"mainOfficeFax": "646546",
-						"managerFullname": null,
-						"describtion": "choonak desc",
-						"languageDTO": {
-							"id": 2,
-							"code": "en",
-							"title": "English"
+                body: JSON.stringify({
+                    "id": this.props.location.state.user_id
+                })
+            });
+            const response = await rawResponse.json();
+            console.log(response);
+            if (response.status == 200 ){
+				// it should be gotten from sth like localstorage
+				const currentlanguage = 'fa'
+				// it should be gotten from sth like localstorage
+				let nameinCurrentLang = {}
+				response.result.dto.productproviderDTO.productproviderLangDTOS.map(each => {	
+					if (currentlanguage == each.languageDTO.code){
+						nameinCurrentLang = {
+							lang_code: each.languageDTO.code,
+							nameinthisLang: each.name,
+							companynameinthislang: each.companyName,
+							descriptioninthislang: each.describtion
 						}
 					}
-				]}}
-		  let dtos_ = the_data.result.dtos
-		  this.suggestions = dtos_.map(eachOwner => {
-			return({label: eachOwner.name, value: eachOwner.name, id: eachOwner.productProviderId})
-		  })
-		  console.log('suggestions', this.suggestions);
-	}
+				})
+
+                this.setState({
+                    username: response.result.dto.username,
+                    familyanme: response.result.dto.fullName,
+                    email: response.result.dto.email,
+                    mobileNumber: response.result.dto.mobile,
+                    description: response.result.dto.description,
+                    productownerid: response.result.dto.productproviderDTO.id,
+					productownerName: nameinCurrentLang.nameinthisLang
+                })
+            }
+		})();
+		
+		(async () => {
+			const rawResponse = await fetch(AppConfig.baseURL + '/product/provider/filter', {
+				method: 'POST',
+				headers: {
+				  'Content-Type': 'application/json',
+				  'Authorization': giventoken
+				},
+				body: JSON.stringify({
+					"fromDate": "",
+					"needPaginate": true,
+					"pageNumber": 0,
+					"pageSize": 10,
+					"resultSize": 0,
+					"termToFind": "",
+					"toDate": ""
+				})
+			});
+			const content = await rawResponse.json();
+			console.log(content);
+			if (content.status == 200 ){
+				console.log('success');
+				let dtos_ = content.result.dtos
+				const suggestions = dtos_.map(eachOwner => {
+				  return({label: eachOwner.name, value: eachOwner.name, id: eachOwner.productProviderId})
+				})
+				this.setState({suggestions})
+			}
+		})();
+    } 
 
 	handleChange = (event) => {
 		const {name, value} = event.target
@@ -249,16 +124,14 @@ export default class AutoComplete extends Component {
 
 	handleChangeOnOwner = (result) => {
 		result == 0 && this.setState({productownerid: 0})
-		this.suggestions.map( owner => {
-			owner.value == result ? this.setState({productownerid: owner.id}) : null
+		this.state.suggestions.map( owner => {
+			owner.value == result && this.setState({productownerid: owner.id})
 		})
 	}
 
 	handleSubmit = (e) => {
-		// e.preventDefault()
-		console.log('submit');
-		const toBsentData = 
-        {
+		e.preventDefault()
+		const toBsentData = {
             "id": this.props.location.state.user_id,
             "username": this.state.username,
             "description": this.state.description,
@@ -268,30 +141,28 @@ export default class AutoComplete extends Component {
             "productproviderDTO": {
                 "id": this.state.productownerid
             }
-        }
-		// (async () => {
-		//   const rawResponse = await fetch(AppConfig.baseURL + '/product/provider/filter', {
-		//       method: 'POST',
-		//       headers: {
-		//         'Content-Type': 'application/json',
-		//         'Authorization': givenToken
-		//       },
-		//       body: JSON.stringify(toBsentData)
-		//   });
-		//   const content = await rawResponse.json();
-		//   console.log(content);
-		//   if (content.status == 200 ){
-		//       console.log('success');
-		//       let dtos_ = content.result.dtos
-		//       suggestions = dtos_.map(eachOwner => {
-		//         return({label: eachOwner.name, value: eachOwner.name, id: eachOwner.productProviderId})
-		//       })
-		//   }
-		// })();
+		}
+		console.log(toBsentData);
+		
+		(async () => {
+		  const rawResponse = await fetch(AppConfig.baseURL + '/permission/user/edit', {
+		      method: 'POST',
+		      headers: {
+		        'Content-Type': 'application/json',
+		        'Authorization': giventoken
+		      },
+		      body: JSON.stringify(toBsentData)
+		  });
+		  const content = await rawResponse.json();
+		  console.log(content);
+		  if (content.status == 200 ){
+		      console.log('success');
+		      this.forceUpdate()
+		  }
+		})();
 	}
 
 	render() {
-		this.beforeMount()
 		return (
 			<div className="formelements-wrapper">
 				<div className="row">
@@ -325,7 +196,7 @@ export default class AutoComplete extends Component {
 								<FormGroup row>
 									<Label for="Product_Owner" sm={2}>Product_Owner</Label>
 									<Col sm={10}>
-										<ReactSelect defaultValue={this.state.productownerName} id={'Product_Owner'} changeHandler={this.handleChangeOnOwner} suggestions={this.suggestions} />
+										<ReactSelect defaultValue={this.state.productownerName} id={'Product_Owner'} changeHandler={this.handleChangeOnOwner} suggestions={this.state.suggestions} />
 									</Col>
 								</FormGroup>
 								<FormGroup row>
@@ -348,7 +219,7 @@ export default class AutoComplete extends Component {
 									</Col>
 								</FormGroup>
 								<FormGroup check className="p-0">
-									<Button color="primary">Submit</Button>
+									<Button onClick={this.handleSubmit} color="primary">Submit</Button>
 								</FormGroup>
 							</Form>
 						</RctCollapsibleCard>
