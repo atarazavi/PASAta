ec9bd696d44a832ebf164ff6995222f563941ae6
ata.razavi <ata.razavi89@gmail.com>
2019-08-01 06:45:06 +0430

    Final Corrections for TagPoolList
    Implementing Pagination for this page

--- src/routes/tagPoolList/index.js
+++ src/routes/tagPoolList/index.js
@@ -5,8 +5,8 @@ import React from 'react';
 import MUIDataTable from "mui-datatables";
 import { Form, FormGroup, Label, Input } from 'reactstrap';
 import Button from '@material-ui/core/Button';
-
-import { Badge } from 'reactstrap';
+import Pagination from 'rc-pagination';
+import './paginatestyle.css'
 // page title bar
 import PageTitleBar from 'Components/PageTitleBar/PageTitleBar';
 
@@ -17,12 +17,10 @@ import RctCollapsibleCard from 'Components/RctCollapsibleCard/RctCollapsibleCard
 import IntlMessages from 'Util/IntlMessages';
 
 import IconButton from '@material-ui/core/IconButton';
-import { Route, Redirect } from "react-router-dom";
 
 import Tooltip from '@material-ui/core/Tooltip';
 // app config
-import AppConfig from '../../constants/AppConfig';
-import { callbackify } from 'util';
+import AppConfig from 'Constants/AppConfig';
 
 class DataTable extends React.Component {
 	state = {
@@ -31,7 +29,10 @@ class DataTable extends React.Component {
         chosennumofresults: 10,
 		thetagPoolList: [],
 		theTagQRCODEs:[{id:null, url:null}],
-		isQRCodeLoaded: false
+		isQRCodeLoaded: false,
+		pagination: false,
+		paginationTotal: 0,
+		paginationCurrentpage: 1
 	}
 	componentDidMount = () => {		
         this.callMainAPI()
@@ -40,6 +41,24 @@ class DataTable extends React.Component {
 		this.callMainAPI()
 	}
 	callMainAPI = () => {
+		const toBsentData = {
+			"fromDate": "",
+			"needPaginate": true,
+			"pageNumber": this.state.paginationCurrentpage,
+			"pageSize": this.state.chosennumofresults,
+			"productProviderId": 0,
+			"resultSize": 0,
+			"tagBulkorderId": 0,
+			"tagPackageId": this.props.location.state.package_id,
+			"tagPackageSeqEnd": this.state.tagPackageSeqEnd,
+			"tagPackageSeqStart": this.state.tagPackageSeqStart,
+			"tagProviderId": 0,
+			"tagRequestId": 0,
+			"tagTypeId": 0,
+			"toDate": ""
+		}
+		console.log('toBsentData', toBsentData);
+		
 		(async () => {
 			const rawResponse = await fetch(AppConfig.baseURL + '/tag/pool/filter', {
 				method: 'POST',
@@ -48,25 +67,12 @@ class DataTable extends React.Component {
 					'Authorization': localStorage.getItem('given_token'),
 					'Accept-Language': localStorage.getItem('Current_lang')
 				},
-				body: JSON.stringify({
-                    "fromDate": "",
-                    "needPaginate": true,
-                    "pageNumber": 0,
-                    "pageSize": this.state.chosennumofresults,
-                    "productProviderId": 0,
-                    "resultSize": 0,
-                    "tagBulkorderId": 0,
-                    "tagPackageId": this.props.location.state.package_id,
-                    "tagPackageSeqEnd": this.state.tagPackageSeqEnd,
-                    "tagPackageSeqStart": this.state.tagPackageSeqStart,
-                    "tagProviderId": 0,
-                    "tagRequestId": 0,
-                    "tagTypeId": 0,
-                    "toDate": ""
-                })
+				body: JSON.stringify(toBsentData)
 			});
 			const response = await rawResponse.json();
 			if (response.status == 200 ){
+				console.log('response.result.paginateModel', response.result.paginateModel);
+				
 				const thetagPoolList = response.result.dtos.map(each => {
                     return({
                         id: each.id,
@@ -75,10 +81,22 @@ class DataTable extends React.Component {
                         theQRCodeurl: ''
                     })
 				})
-				this.setState(
-					{thetagPoolList}
-				)
-				this.handleGettingQRCode()
+				if (response.result.paginateModel.pages.length > 0) {
+					this.setState(
+						{
+						pagination: true,
+						paginationTotal: response.result.paginateModel.pages.length,
+						thetagPoolList
+						},
+						()=> this.handleGettingQRCode()
+					)
+				}else{
+					this.setState(
+						{thetagPoolList},
+						()=> this.handleGettingQRCode()
+					)
+				}
+				
 			}
         })();
     }
@@ -121,9 +139,19 @@ class DataTable extends React.Component {
 			[name]: value
 		})
 	}
-
+	paginatehandler = (SelectedPage,PageSize) => {
+		console.log('SelectedPage', SelectedPage);
+		console.log('PageSize', PageSize);
+		this.setState({
+			paginationCurrentpage: SelectedPage
+		}, () => {
+			this.callMainAPI();
+		});
+		
+	}
 	render() {  
 		const columns = ["QR Code", "Tag ID", "Priority in Package", "Action"];
+		const columnsfa = ["QR Code", "شناسه تگ", "اولویت در بسته", "اقدامات"];
 		const data = this.state.thetagPoolList.map(each => { 
 			let eachURL;
 			this.state.theTagQRCODEs.map(eachQR => {
@@ -140,8 +168,15 @@ class DataTable extends React.Component {
 			)
 		})
 		const options = {
-			filterType: 'dropdown',
-			responsive: 'stacked'
+			filter: false,
+			responsive: 'scroll',
+			selectableRows: false,
+			download: false,
+			print: false,
+			search: false,
+			viewColumns: false,
+			sort: true,
+			pagination: false
 		};
         const ViewNumberOptions = []
         for(let i = 0; i< AppConfig.toBshownResultNumberOptions.length; i++){
@@ -149,32 +184,46 @@ class DataTable extends React.Component {
         }  
 		return (
 			<div className="data-table-wrapper">
-				<PageTitleBar title='لیست برچسب های درخواست شده' match={this.props.match} />
-				<RctCollapsibleCard heading="لیست درخواست ها" fullBlock>
-						<div className="top-filter clearfix p-20">
-							<FormGroup className="w-20">
-								<Label for="tagPackageIdStart">ترتیب پکیج از:</Label>
-								<Input onChange={this.handleChange} type="text" name="tagPackageIdStart" id="tagPackageIdStart" placeholder="Tag Package ID Start from" />							
-							</FormGroup>
-							<FormGroup className="w-20">
-								<Label for="tagPackageIdEnd">تا:</Label>
-								<Input onChange={this.handleChange} type="text" name="tagPackageIdEnd" id="tagPackageIdEnd" placeholder="Tag Package ID Start to" />							
-							</FormGroup>
-							<FormGroup className="w-20">
-								<Label for="Select">Number of Results:</Label>
-								<Input type="select" onChange={this.handleChange} name="chosennumofresults" id="Select">
-									{ViewNumberOptions}
-								</Input>
-							</FormGroup>
-							<FormGroup className="mb-5">
-								<Label className="d-block">&nbsp;</Label>
-								<Button onClick={this.handleFilterApply} color="primary" variant="raised" className="mr-10 text-white"><IntlMessages id="widgets.apply" /></Button>
-							</FormGroup>
+				<PageTitleBar title={<IntlMessages id="Tag Pool List" />} match={this.props.match} />
+				<div className="report-status mb-30">
+					<div className="row">
+						<div className="col-md-12">
+							<div className="top-filter clearfix p-20">
+								<FormGroup className="w-20">
+									<Label for="tagPackageIdStart"><IntlMessages id="PackageIDStartFrom" />:</Label>
+									<Input onChange={this.handleChange} type="text" name="tagPackageIdStart" id="tagPackageIdStart" placeholder="Tag Package ID Start from" />							
+								</FormGroup>
+								<FormGroup className="w-20">
+									<Label for="tagPackageIdEnd"><IntlMessages id="To" />:</Label>
+									<Input onChange={this.handleChange} type="text" name="tagPackageIdEnd" id="tagPackageIdEnd" placeholder="Tag Package ID Start to" />							
+								</FormGroup>
+								<FormGroup className="w-20">
+									<Label for="Select"><IntlMessages id="numberofresults" />:</Label>
+									<Input type="select" onChange={this.handleChange} name="chosennumofresults" id="Select">
+										{ViewNumberOptions}
+									</Input>
+								</FormGroup>
+								<FormGroup className="mb-5">
+									<Label className="d-block">&nbsp;</Label>
+									<Button onClick={this.handleFilterApply} color="primary" variant="raised" className="mr-10 text-white"><IntlMessages id="widgets.apply" /></Button>
+								</FormGroup>
+							</div>
 						</div>
+					</div>
+				</div>
+				<RctCollapsibleCard>
 					<MUIDataTable
-						title={"Users list"}
+						title={this.state.pagination ? 
+							<Pagination 
+								className="ant-pagination" 
+								onChange={this.paginatehandler} 
+								defaultCurrent={this.state.paginationCurrentpage} 
+								total={this.state.chosennumofresults * this.state.paginationTotal}
+								pageSize={this.state.chosennumofresults} 
+							/> 
+						: ''}
 						data={data}
-						columns={columns}
+						columns={localStorage.getItem('Current_lang') == 'en' ? columns : columnsfa}
 						options={options}
 					/>
 				</RctCollapsibleCard>
